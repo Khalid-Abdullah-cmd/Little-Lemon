c7ef6aa3fbe64b2889048e9c887f221d8aa5e99a
Khalid-Abdullah-cmd <khalidabdullahmohammed4@gmail.com>
2026-05-01 20:50:57 +0300

    completed database and api

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -10,6 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'restaurant',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -72,11 +81,14 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'littlelemon',
+        'USER': 'root', 
+        'PASSWORD': os.environ.get('DB_PASSWORD'), 
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from rest_framework import generics, viewsets
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'index.html')
+
+
+
+class MenuItemsView(generics.ListCreateAPIView):
+    
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+
+    
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer    
